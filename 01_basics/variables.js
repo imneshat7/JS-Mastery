234dccaff17e07c5c3be97b2d95d9acdc6619d5e
imneshat7 <119743809+imneshat7@users.noreply.github.com>
2024-11-19 19:07:23 +0000

    Datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId=321
+let email = "belal23@gmail.com"
+var accountPass = "12345"
+accountCity="Jaipur"
+// accountId=2
+email = "neshat23@gmail.com"
+accountPass = "1111"
+accountCity="Patna"
+let accountState;
+console.log([accountId,accountPass,accountCity,email,accountState]);
+
+
+
+
+
+console.log([accountId,email,accountPass,accountCity]);
+
