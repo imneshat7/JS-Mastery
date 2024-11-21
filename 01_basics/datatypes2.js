94287f8460023c687af5edb0b4b17b9377ec9bfd
imneshat7 <119743809+imneshat7@users.noreply.github.com>
2024-11-21 14:09:44 +0000

    Conversion Confusion

---
+++ 01_basics/datatypes2.js
@@ -0,0 +1,15 @@
+// let score = "333"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let isLoggedin = ''
+// let booleanLoggedin = Boolean(isLoggedin)
+// console.log(booleanLoggedin);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( stringNumber);
+
+
+
