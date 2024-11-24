37899a00fd5f78b4eb709897320b9f8f826968ac
imneshat7 <119743809+imneshat7@users.noreply.github.com>
2024-11-24 04:38:43 +0000

    NumbersandMaths

--- 01_basics/datatypes2.js
+++ 01_basics/datatypes2.js
@@ -6,10 +6,12 @@
 // let booleanLoggedin = Boolean(isLoggedin)
 // console.log(booleanLoggedin);
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log( stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log( stringNumber);
 
 
-
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId);
