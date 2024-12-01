f8ddadf4e2e4a55a129b48e88864ae5a91494dfc
imneshat7 <119743809+imneshat7@users.noreply.github.com>
2024-12-01 07:04:33 +0000

    switch

---
+++ Control_flow/switch.js
@@ -0,0 +1,21 @@
+const month="March"
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        break;
+}
