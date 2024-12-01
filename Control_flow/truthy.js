6c2d58a709f1ff8293a6093574cf9675466cafd4
imneshat7 <119743809+imneshat7@users.noreply.github.com>
2024-12-01 07:04:49 +0000

    truthy

---
+++ Control_flow/truthy.js
@@ -0,0 +1,20 @@
+// const userEmail=[]
+// if (userEmail.length === 0) {
+//     console.log("Useremail is empty");
+    
+// }
+
+const emptyObj={}
+if (Object.keys(emptyObj).length === 0) {
+    console.log("object is empty");
+    
+}
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10
+console.log(val1);
+
+
